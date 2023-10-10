206552e6d755041bdd0f612026a84b8de44ab1e9
amir.mlk89 <amirudin.mlk@gmail.com>
2023-10-10 16:58:37 +0800

    DATA BARU

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Laman Utama') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,9 +14,53 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    Tahniah anda telah berjaya log masuk
                 </div>
             </div>
+            <div class="col-md-8">
+            <div class="card mt-2">
+                <div class="card-header">{{ __('Data Agensi') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">BIL</th>
+                            <th scope="col">JABATAN</th>
+                            <th scope="col">KOD JABATAN</th>
+                            <th scope="col">EMAIL RASMI</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">1</th>
+                            <td>JKM</td>
+                            <td>0030</td>
+                            <td>@mdo</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">2</th>
+                            <td>JKPNM</td>
+                            <td>0140</td>
+                            <td>@fat</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">PERTANIAN</td>
+                            <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            
         </div>
     </div>
 </div>
